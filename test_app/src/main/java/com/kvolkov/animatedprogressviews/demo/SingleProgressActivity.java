01d959fee5c7b70cee75e51ad681fcead2209a96
Kirill Volkov <vulkovk@gmail.com>
2017-03-30 13:41:43 +0200

    + Added support for multicolored arcs to demo app. Some refactoring and bugfix.

--- test_app/src/main/java/com/kvolkov/animatedprogressviews/demo/SingleProgressActivity.java
+++ test_app/src/main/java/com/kvolkov/animatedprogressviews/demo/SingleProgressActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 
@@ -15,6 +17,51 @@ import java.util.List;
 
 public class SingleProgressActivity extends AppCompatActivity {
 
+    // colors for demo
+    private static List<Integer> sColorList = new ArrayList<>();
+    static {
+        sColorList.add(Color.RED);
+        sColorList.add(Color.BLUE);
+        sColorList.add(Color.GREEN);
+        sColorList.add(Color.GRAY);
+        sColorList.add(Color.CYAN);
+        sColorList.add(Color.MAGENTA);
+        sColorList.add(Color.YELLOW);
+        sColorList.add(Color.DKGRAY);
+        sColorList.add(Color.RED);
+        sColorList.add(Color.BLUE);
+        sColorList.add(Color.GREEN);
+        sColorList.add(Color.GRAY);
+        sColorList.add(Color.CYAN);
+        sColorList.add(Color.MAGENTA);
+        sColorList.add(Color.YELLOW);
+        sColorList.add(Color.DKGRAY);
+        sColorList.add(Color.RED);
+        sColorList.add(Color.BLUE);
+        sColorList.add(Color.GREEN);
+        sColorList.add(Color.GRAY);
+        sColorList.add(Color.CYAN);
+        sColorList.add(Color.MAGENTA);
+        sColorList.add(Color.YELLOW);
+        sColorList.add(Color.DKGRAY);
+        sColorList.add(Color.RED);
+        sColorList.add(Color.BLUE);
+        sColorList.add(Color.GREEN);
+        sColorList.add(Color.GRAY);
+        sColorList.add(Color.CYAN);
+        sColorList.add(Color.MAGENTA);
+        sColorList.add(Color.YELLOW);
+        sColorList.add(Color.DKGRAY);
+        sColorList.add(Color.RED);
+        sColorList.add(Color.BLUE);
+        sColorList.add(Color.GREEN);
+        sColorList.add(Color.GRAY);
+        sColorList.add(Color.CYAN);
+        sColorList.add(Color.MAGENTA);
+        sColorList.add(Color.YELLOW);
+        sColorList.add(Color.DKGRAY);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,10 +71,26 @@ public class SingleProgressActivity extends AppCompatActivity {
 
         final Spinner animationTypeSelector = (Spinner) findViewById(R.id.animationTypeSelector);
         final Spinner opacityAnimationTypeSelector = (Spinner) findViewById(R.id.opacityAnimationTypeSelector);
+        final CheckBox useColorsCB = (CheckBox) findViewById(R.id.useColorListCB);
         final SeekBar arcNumSeekBar = (SeekBar) findViewById(R.id.arcCountBar);
         final SeekBar arcStrokeWidthSeekBar = (SeekBar) findViewById(R.id.arcStrokeWidthBar);
         final SeekBar arcPaddingSeekBar = (SeekBar) findViewById(R.id.arcPaddingBar);
         final ArcLoadingView progressView = (ArcLoadingView) findViewById(R.id.progress);
+
+        useColorsCB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    List<Integer> colorList = new ArrayList<>();
+                    for (int i = 0; i < progressView.getArcCount(); i++) {
+                        colorList.add(sColorList.get(i));
+                    }
+                    progressView.setColorList(colorList);
+                } else {
+                    progressView.setColorList(null);
+                }
+            }
+        });
 
         animationTypeSelector.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
             @Override
@@ -62,6 +125,15 @@ public class SingleProgressActivity extends AppCompatActivity {
                     for (int i = 0; i < progress; i++) {
                         int channelVal = Math.round(255.f * i / progress);
                         colorList.add(Color.argb(channelVal, channelVal, channelVal, channelVal));
+                    }
+                    progressView.setColorList(colorList);
+                }
+
+                // update color list when num arcs changed
+                if (useColorsCB.isChecked()) {
+                    List<Integer> colorList = new ArrayList<>();
+                    for (int i = 0; i < progressView.getArcCount(); i++) {
+                        colorList.add(sColorList.get(i));
                     }
                     progressView.setColorList(colorList);
                 }
